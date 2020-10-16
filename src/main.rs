98bc0cad42994e955dc0d44a7d5d6d06e864177f
Nicholas Buckeridge <bucknich@gmail.com>
2020-10-17 09:19:07 +1000

    ➕ Added some structures

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
-// use actix_web::{middleware, web, App, HttpRequest, HttpServer};
+use actix_web::{
+    error, middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer,
+};
+use futures::StreamExt;
+use json::JsonValue;
+use serde::{Deserialize, Serialize};
 
+struct Database {
+    users: Vec<User>,
+    homes: Vec<Home>,
+}
+
+struct User {
+    id: usize,
+    first_name: String,
+    last_name: String,
+    email: String,
+    password: String,
+    member_to: Vec<usize>, // Home.id
+}
+
+struct Home {
+    id: usize,
+    owner_id: usize, // User.id
+    members: Vec<usize>, // [User.id]
+    features: Vec<Feature>,
+}
+
+struct Feature {
+
+}
 
 
 
