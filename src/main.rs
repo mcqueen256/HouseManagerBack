6ab1d246bbc84847fdefee44db9bb99a7f8be959
Nicholas Buckeridge <bucknich@gmail.com>
2020-10-17 10:25:22 +1000

    🤩 Did I just finish my backend?

--- src/main.rs
+++ src/main.rs
@@ -47,24 +47,13 @@ struct Feature {
 
 }
 
-/// This handler uses json extractor
-async fn index(item: web::Json<Database>) -> HttpResponse {
-    println!("model: {:?}", &item);
-    HttpResponse::Ok().json(item.0) // <- send response
-}
-
-// async fn push(dababase: web::Json<<Database>) -> HttpResponse {
-//     println!("updating database...");
-//     let file = fs::File::open("db.json")
-//     let file = if let file = fs::File::open(path) 
-// }
 
 use std::io::Write;
 async fn pop() -> HttpResponse {
     println!("sending database...");
     let contents= fs::read_to_string(DATABASE).unwrap_or_else(|e| {
         eprintln!("Error: {:?}", e);
-        eprintln!("Creating a new file...");
+        eprintln!("Creating (a) a new file...");
         let mut file = fs::File::create(DATABASE).expect("could not create new file db.json");
         let contents = serde_json::to_string(&Database::new()).expect("coule not create a json from Database::new()");
         file.write_all(contents.as_bytes()).expect("could not write to file");
@@ -72,6 +61,18 @@ async fn pop() -> HttpResponse {
     });
     let db: Database = serde_json::from_str(&contents).expect("could not build json from Database struct");
     HttpResponse::Ok().json(db) // <- send response
+}
+
+async fn push(db: web::Json<Database>) -> HttpResponse {
+    println!("saving database...");
+    let mut file = fs::OpenOptions::new()
+        .create(true)
+        .write(true)
+        .open(DATABASE)
+        .expect("could not open file for writing");
+    let contents = serde_json::to_string(&db.0).expect("failed to convert recieved object to json str");
+    file.write_all(contents.as_bytes()).expect("could not write to file");
+    HttpResponse::Ok().finish() // <- send response
 }
 
 #[actix_web::main]
@@ -84,16 +85,8 @@ async fn main() -> std::io::Result<()> {
             // enable logger
             .wrap(middleware::Logger::default())
             .data(web::JsonConfig::default().limit(4096)) // <- limit size of the payload (global configuration)
-            // .service(web::resource("/extractor").route(web::post().to(index)))
-            // .service(
-            //     web::resource("/extractor2")
-            //         .data(web::JsonConfig::default().limit(1024)) // <- limit size of the payload (resource level)
-            //         .route(web::post().to(extract_item)),
-            // )
-            // .service(web::resource("/manual").route(web::post().to(index_manual)))
-            // .service(web::resource("/mjsonrust").route(web::post().to(index_mjsonrust)))
-            .service(web::resource("/").route(web::post().to(index)))
             .service(web::resource("/pop").route(web::post().to(pop)))
+            .service(web::resource("/push").route(web::post().to(push)))
     })
     .bind("127.0.0.1:8080")?
     .run()
